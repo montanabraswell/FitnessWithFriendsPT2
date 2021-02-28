c6abd6b01d34f07ef1fcb4a92653b28c4577c7fa
Montana <montana@Montanas-MacBook-Pro.local>
2021-02-27 23:56:03 -0500

    fixed height constraint to remove error

---
+++ FitnessWithFriends/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FitnessWithFriends
+//
+//  Created by Montana  on 2/22/21.
+//  Copyright © 2021 Montana Braswell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
